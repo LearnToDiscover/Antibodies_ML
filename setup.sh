d047a8bde7571963e176a34f3413ffba6ba83ad5
sabaferdous12 <saba.ferdous.13@ucl.ac.uk>
2022-12-13 15:37:38 +0000

    Requirements files added

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+
+pip install jupytext
+
+pip install nibabel
+
+mkdir -p ~/.virtualenvs/
+
+python3 -m venv ~/.virtualenvs/r-env
+source ~/.virtualenvs/r-env/bin/activate
+
+pip install -r requirements.txt
+
+sudo apt update -y
+
+sudo apt install -y texlive-latex-base texlive-fonts-recommended texlive-fonts-extra texlive-latex-extra
+
